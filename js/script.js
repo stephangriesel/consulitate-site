9307dc6afa5b5077ab79bf4842393f270654eacf
Stephan <sgriesel@gmail.com>
2020-09-28 06:50:36 +0200

    prepare logic structure - wip

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Scroll to top
+
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
@@ -12,4 +14,62 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Carousel
+
+$(function () {
+  var prevIndex = 0;
+  var currentIndex = 1;
+  var nextIndex = 2;
+  var lastIndex = $('#clients-carousel').find('client').length - 1;
+
+  $('#clients-carousel').on('click', '.previous', showClient);
+  $('#clients-carousel').on('click', '.next', showClient);
+  $('#client-carousel-pips').on('click', '.pip', showFromPip);
+
+  generatePips();
+
+  var carouselRunning = true;
+  var interval = setInterval(function () {
+    if (carouselRunning) {
+      showNextClient();
+    }
+  }, 4000);
+
+  function showNextClient() {
+
+  };
+
+  function showClient() {
+
+  };
+
+  function updateState(index) {
+
+  }
+
+  function updateCarouselPosition() {
+
+  }
+
+  function generatePips() {
+
+  }
+
+  function updatePips() {
+
+  }
+
+  function showFromPip() {
+
+  }
+
+  function setLeftClass() {
+
+  }
+
+});
+
+
+
 
