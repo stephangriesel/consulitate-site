e38cb1e44dabd758d4fcb09c0526dc840c77690c
Stephan <sgriesel@gmail.com>
2020-09-29 07:14:53 +0200

    update logic & fix position of tiles

--- js/script.js
+++ js/script.js
@@ -21,15 +21,17 @@ $(function () {
   var prevIndex = 0;
   var currentIndex = 1;
   var nextIndex = 2;
-  var lastIndex = $('#clients-carousel').find('client').length - 1;
+  var lastIndex = $('#clients-carousel').find('.client').length - 1;
 
   $('#clients-carousel').on('click', '.previous', showClient);
   $('#clients-carousel').on('click', '.next', showClient);
   $('#client-carousel-pips').on('click', '.pip', showFromPip);
 
   generatePips();
+  setLeftClass();
 
   var carouselRunning = true;
+
   var interval = setInterval(function () {
     if (carouselRunning) {
       showNextClient();
@@ -37,18 +39,43 @@ $(function () {
   }, 4000);
 
   function showNextClient() {
-
+    if (currentIndex === lastIndex) {
+      currentIndex = 0;
+    } else {
+      currentIndex++;
+    }
+    updateState(currentIndex);
   };
 
-  function showClient() {
-
+  function showClient(event) {
+    if ($(event.target).hasClass('client')) {
+      var target = $(event.target);
+    } else {
+      var target = $(event.target).parent();
+    }
+    var index = $('.client').index(target);
+    updateState(index);
   };
 
   function updateState(index) {
+    prevIndex = index === 0 ? lastIndex : index - 1;
+    currentIndex = index;
+    nextIndex = index === lastIndex ? 0 : index + 1;
 
+    updateCarouselPosition();
+    setLeftClass();
+    updatePips();
   }
 
   function updateCarouselPosition() {
+    $('#clients-carousel').find('.previous').removeClass('previous');
+    $('#clients-carousel').find('.current').removeClass('current');
+    $('#clients-carousel').find('.next').removeClass('next');
+
+    var allClients = $('#clients-carousel').find('.client');
+    $(allClients[prevIndex]).addClass('previous');
+    $(allClients[currentIndex]).addClass('current');
+    $(allClients[nextIndex]).addClass('next');
 
   }
 
